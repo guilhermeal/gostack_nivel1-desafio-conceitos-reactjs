822a834cab47fc54ed52f7cf5021ec048ec24cae
Guilherme Almeida <guiallan.al@gmail.com>
2020-08-23 04:04:46 -0300

    Adjust routes to create and remove projects from frontend to backend
    Ref: Challange#3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from "react";
+import api from './services/api'
 
 import "./styles.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  function didMount() {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data)
+    });
+  }
+  useEffect(() => {
+    didMount();
+  }, [])
+  
   async function handleAddRepository() {
-    // TODO
+    
+    const response = await api.post('/repositories', {
+      url: 'https://github.com/guilhermeal/NLW-2_Rocketseat',
+      title: `${Date.now()} Projetos e códigos construídos na Next Level Week #2 da Rocketseat.`,
+      techs: ['Javascript', 'Typescript', 'NodeJS', 'HTML', 'CSS']
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  async function handleRemoveRepository(id) {    
+    api.delete(`/repositories/${id}`);
+    setRepositories(repositories.filter(repository => repository.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => 
+          <li key={repository.id} >
+            <a href={repository.url}>{repository.title}</a>
+            
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
